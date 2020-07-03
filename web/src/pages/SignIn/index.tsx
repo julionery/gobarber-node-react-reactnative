badd4624efb4d84c6c226ce6b41eeebff77a4e37
Júlio Nery <julio_cesar.an@hotmail.com>
2020-07-02 21:59:26 -0300

    finalizando estrutura login e cadastro

---
+++ web/src/pages/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="">
+        <FiLogIn /> Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
