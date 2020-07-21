b6ce90be828f74d9107f989b2147e9401eb88667
Júlio Nery <julio_cesar.an@hotmail.com>
2020-07-20 22:24:46 -0300

    reorganizando a estrutura do projeto

--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
+import AppError from '@shared/errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
-import AppError from '../errors/AppError';
 
 interface RequestDTO {
   provider_id: string;
