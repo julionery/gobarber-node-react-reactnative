1c5c520a3f1c4fcc2f31d8b0dc84c637f1464467
Júlio Nery <julio_cesar.an@hotmail.com>
2020-08-05 01:27:48 -0300

    general api adjustments

--- server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -14,9 +14,12 @@ class AppointmentsRepository implements IAppointmentsRepository {
     this.ormRepository = getRepository(Appointment);
   }
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
     const findAppointment = await this.ormRepository.findOne({
-      where: { date },
+      where: { date, provider_id },
     });
     return findAppointment || undefined;
   }
@@ -58,6 +61,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
--- server/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ server/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
         { provider_id, year, month, day },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
